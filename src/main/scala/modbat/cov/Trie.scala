4155a20d3a3a744fe2f5395cc5651def4c6ee81e
Rui Wang <wruiwr86@gmail.com>
2018-10-10 16:15:41 +0200

    record how many times that transition occurs into the trie

--- src/main/scala/modbat/cov/Trie.scala
+++ src/main/scala/modbat/cov/Trie.scala
@@ -12,17 +12,21 @@ class Trie {
     for (p <- pathInfo) {
       var node:TrieNode = currentNode.children.getOrElse(p._3.toString(),null)
       if (currentNode.previousTransition != null && currentNode.previousTransition == p._3) {
-        currentNode.sameTransCounter += 1
-       Log.info("the transition stored in trie node:" + p._3.toString() + " counter:" + currentNode.sameTransCounter)
+        currentNode.selfTransCounter += 1
+       Log.info("the transition stored in trie node:" + p._3.toString() + " counter:" + currentNode.selfTransCounter)
       }else {
         if (node == null) {
           node = TrieNode()
           node.previousTransition = p._3
           node.modelInfo = (p._1,p._2)
-          node.transitionInfo = (p._3.toString(), p._3.idx)
+          node.transitionInfo = (p._3.toString(), p._3.idx,1)
           currentNode.children.put(p._3.toString(), node)
         }else{
           Log.info("transition already exist in trie:" + node.transitionInfo)
+          if (node.transitionInfo._2 == p._3.idx) {
+            node.transitionInfo = node.transitionInfo.copy(_3 = node.transitionInfo._3 + 1)
+            Log.info ("got a same transition executed: " + node.transitionInfo._3 + " times")
+          }
         }
         currentNode = node
       }
@@ -37,9 +41,9 @@ class Trie {
     for (t <- root.children.keySet) {
       val node = root.children.getOrElse(t,sys.error(s"unexpected key: $t"))
       if (level == 0) {
-        Log.info(node.modelInfo.toString() + node.transitionInfo.toString() + " ("+ node.sameTransCounter +")")
+        Log.info(node.modelInfo.toString() + node.transitionInfo.toString() + " ("+ node.selfTransCounter +")")
       }
-      else Log.info("-"*level + node.modelInfo.toString() + node.transitionInfo.toString() + " ("+ node.sameTransCounter +")")
+      else Log.info("-"*level + node.modelInfo.toString() + node.transitionInfo.toString() + " ("+ node.selfTransCounter +")")
       display(node,level+1)
     }
   }
@@ -60,8 +64,8 @@ class Trie {
 case class TrieNode() {
   var children: HashMap[String,TrieNode] = HashMap.empty[String,TrieNode] // children stores the transition in string and the next node
   var isLeaf: Boolean = false
-  var sameTransCounter = 1
+  var selfTransCounter = 1
   var previousTransition:Transition = null
   var modelInfo: (String, Int) = _ // modelInfo stores the name of the model and its id
-  var transitionInfo: (String, Int) = _ // transitionInfo store the transition and its id
+  var transitionInfo: (String, Int, Int) = _ // transitionInfo store the transition, its id, and the times that transition are executed
 }
