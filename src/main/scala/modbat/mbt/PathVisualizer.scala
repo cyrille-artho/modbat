9a36885a00df31114410adb0e95e9342b6ce0e29
Rui Wang <wruiwr86@gmail.com>
2018-10-18 00:05:20 +0200

    fixed problem for output graph for the Point style

--- src/main/scala/modbat/mbt/PathVisualizer.scala
+++ src/main/scala/modbat/mbt/PathVisualizer.scala
@@ -1,8 +1,13 @@
 package modbat.mbt
 
 import java.io.{File, FileOutputStream, IOException, PrintStream}
+
 import modbat.cov.{Trie, TrieNode}
 import modbat.log.Log
+
+import scala.collection.mutable.ListBuffer
+
+case class LabelInfo (label:String, self:Boolean = false)
 
 class PathVisualizer(trie:Trie, shape:String) {
   require(shape == "Box" || shape == "Point", "the input of path visualizer must be either Box or Point")
@@ -25,12 +30,31 @@ class PathVisualizer(trie:Trie, shape:String) {
     out.println("  graph [ rankdir = \"TB\", ranksep=\"0.3\", nodesep=\"0.2\" ];")
     out.println("  node [ fontname = \"Helvetica\", fontsize=\"6.0\", shape=\""+ shape.toLowerCase +"\", margin=\"0.07\"," + " height=\"0.1\" ];")
     out.println("  edge [ fontname = \"Helvetica\", fontsize=\"6.0\"," + " margin=\"0.05\" ];")
-    display(trie.root, 0)
+    val stack:ListBuffer[LabelInfo] = new ListBuffer[LabelInfo]
+    display(trie.root, 0, 0, stack)
     out.println("}")
   }
 
-  def display(root:TrieNode, level:Int = 0) {
-    if (root.isLeaf) return
+  def display(root:TrieNode, level:Int = 0, nodeNumber:Int = 0, stack:ListBuffer[LabelInfo] = null):(Int,ListBuffer[LabelInfo]) = {
+    var newNodeNumber = nodeNumber
+    var newStack = stack
+    if (root.isLeaf) {
+      if (newStack != null) {
+        for (i <- newStack.indices){
+          newNodeNumber = newNodeNumber + 1
+          if (i == 0) out.println(i + "->" + newNodeNumber + newStack(i).label)
+          else if (newStack(i).self){
+            newNodeNumber = newNodeNumber -1
+            out.println(newNodeNumber + "->" + newNodeNumber + newStack(i).label)
+          }
+          else out.println(newNodeNumber -1 + "->" + newNodeNumber + newStack(i).label)
+        }
+      }
+      if (newStack != null){
+        newStack.trimEnd(1)
+      }
+      return (newNodeNumber, newStack)
+    }
     for (t <- root.children.keySet) {
       val node = root.children.getOrElse(t,sys.error(s"unexpected key: $t"))
       val modelName = node.modelInfo.modelName
@@ -46,15 +70,23 @@ class PathVisualizer(trie:Trie, shape:String) {
         "Transition Execution Counter:" + transExecutionCounter  + "\\n" +
         selfTransCounter + "\"];"
       shape.toLowerCase match {
-        case "box" => val nodeIDFrom = transName.split("=>")(0)
-                      val nodeIDTo = transName.split("=>")(1).stripSuffix(" (1)")
+        case "box" => val nodeIDFrom = transName.split(" => ")(0)
+                      val nodeIDTo = transName.split(" => ")(1).stripSuffix(" (1)")
                       out.println("  " + nodeIDFrom + "->" + nodeIDTo  + newlabel)
-        case "point" => val nodeIDFrom = node.transitionInfo.transitionID -1
-                        val nodeIDTo = node.transitionInfo.transitionID
-                        if (level == 0) out.println("  \"\"" + "->" + node.transitionInfo.transitionID + newlabel)
-                        else out.println("  " + nodeIDFrom + "->" + nodeIDTo + newlabel)
+                       display(node,level+1)
+        case "point" =>
+                        val newlabelInfo = if (transName.split(" => ")(0) == transName.split(" => ")(1).stripSuffix(" (1)")) LabelInfo(newlabel,true)
+                                           else LabelInfo(newlabel)
+                        newStack += newlabelInfo
+                       // Log.info("new stack:" + newStack)
+                        val result = display(node,level+1, newNodeNumber, newStack)
+                        newNodeNumber = result._1
+                        newStack = result._2
       }
-      display(node,level+1)
     }
+    if (newStack != null){
+      newStack.trimEnd(1)
+    }
+    (newNodeNumber, newStack)
   }
 }
