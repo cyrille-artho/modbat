58b6ae12826057461b47fb2373aac4f911afb8a9
Rui Wang <wruiwr86@gmail.com>
2018-10-17 13:12:25 +0200

    refact tuple to class, used to record path info

--- src/main/scala/modbat/mbt/PathVisualizer.scala
+++ src/main/scala/modbat/mbt/PathVisualizer.scala
@@ -4,7 +4,7 @@ import java.io.{File, FileOutputStream, IOException, PrintStream}
 import modbat.cov.{Trie, TrieNode}
 import modbat.log.Log
 
-class PathVisualize(trie:Trie, shape:String) {
+class PathVisualizer(trie:Trie, shape:String) {
   require(shape == "Box" || shape == "Point", "the input of path visualizer must be either Box or Point")
   var out: PrintStream = null
   val outFile = "pathInfoIn" + shape + "Graph.dot"
@@ -23,7 +23,7 @@ class PathVisualize(trie:Trie, shape:String) {
     out.println("digraph model {")
     out.println("  orientation = landscape;")
     out.println("  graph [ rankdir = \"TB\", ranksep=\"0.3\", nodesep=\"0.2\" ];")
-    out.println("  node [ shape=\""+ shape.toLowerCase +"\", margin=\"0.07\"," + " height=\"0.1\" ];")
+    out.println("  node [ fontname = \"Helvetica\", fontsize=\"6.0\", shape=\""+ shape.toLowerCase +"\", margin=\"0.07\"," + " height=\"0.1\" ];")
     out.println("  edge [ fontname = \"Helvetica\", fontsize=\"6.0\"," + " margin=\"0.05\" ];")
     display(trie.root, 0)
     out.println("}")
@@ -33,11 +33,11 @@ class PathVisualize(trie:Trie, shape:String) {
     if (root.isLeaf) return
     for (t <- root.children.keySet) {
       val node = root.children.getOrElse(t,sys.error(s"unexpected key: $t"))
-      val modelName = node.modelInfo._1.toString
-      val modelID = node.modelInfo._2.toString
-      val transName = node.transitionInfo._1.toString.stripSuffix(" (1)")
-      val transID = node.transitionInfo._2.toString
-      val transExecutionCounter = node.transitionInfo._3.toString
+      val modelName = node.modelInfo.modelName
+      val modelID = node.modelInfo.modelID.toString
+      val transName = node.transitionInfo.transitionName.stripSuffix(" (1)")
+      val transID = node.transitionInfo.transitionID.toString
+      val transExecutionCounter = node.transitionInfo.transCounter.toString
       val selfTransCounter = "(Transition Self-execution Times:"+ node.selfTransCounter +")"
       val newlabel =  "[label = \"" + "Model Name:" + modelName  + "\\n" +
         "Model ID:" + modelID + "\\n" +
@@ -46,12 +46,12 @@ class PathVisualize(trie:Trie, shape:String) {
         "Transition Execution Counter:" + transExecutionCounter  + "\\n" +
         selfTransCounter + "\"];"
       shape.toLowerCase match {
-        case "box" => val nodeIDFrom = node.transitionInfo._1.split("=>")(0)
-                      val nodeIDTo = node.transitionInfo._1.split("=>")(1).stripSuffix(" (1)")
+        case "box" => val nodeIDFrom = transName.split("=>")(0)
+                      val nodeIDTo = transName.split("=>")(1).stripSuffix(" (1)")
                       out.println("  " + nodeIDFrom + "->" + nodeIDTo  + newlabel)
-        case "point" => val nodeIDFrom = node.transitionInfo._2 -1
-                        val nodeIDTo = node.transitionInfo._2
-                        if (level == 0) out.println("  \"\"" + "->" + node.transitionInfo._2 + newlabel)
+        case "point" => val nodeIDFrom = node.transitionInfo.transitionID -1
+                        val nodeIDTo = node.transitionInfo.transitionID
+                        if (level == 0) out.println("  \"\"" + "->" + node.transitionInfo.transitionID + newlabel)
                         else out.println("  " + nodeIDFrom + "->" + nodeIDTo + newlabel)
       }
       display(node,level+1)
