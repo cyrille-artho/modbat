f6924e0e1b7efa8c8f7077c76b03ec42cdd431a1
Rui Wang <wruiwr86@gmail.com>
2018-10-18 16:28:54 +0200

    merged same transition in the box graph

--- src/main/scala/modbat/mbt/PathVisualizer.scala
+++ src/main/scala/modbat/mbt/PathVisualizer.scala
@@ -7,7 +7,8 @@ import modbat.log.Log
 
 import scala.collection.mutable.ListBuffer
 
-case class LabelInfo (label:String, self:Boolean = false)
+case class LabelInfo (label:String, self:Boolean = false) // LabelInfo is used for record the label information used for "point" output
+case class NodeInfo (node:TrieNode, var transCounter:String) // NodeInfo is used for recode  the node information used for "box" output
 
 class PathVisualizer(trie:Trie, shape:String) {
   require(shape == "Box" || shape == "Point", "the input of path visualizer must be either Box or Point")
@@ -30,30 +31,33 @@ class PathVisualizer(trie:Trie, shape:String) {
     out.println("  graph [ rankdir = \"TB\", ranksep=\"0.3\", nodesep=\"0.2\" ];")
     out.println("  node [ fontname = \"Helvetica\", fontsize=\"6.0\", shape=\""+ shape.toLowerCase +"\", margin=\"0.07\"," + " height=\"0.1\" ];")
     out.println("  edge [ fontname = \"Helvetica\", fontsize=\"6.0\"," + " margin=\"0.05\" ];")
-    val stack:ListBuffer[LabelInfo] = new ListBuffer[LabelInfo]
-    display(trie.root, 0, 0, stack)
+    val labelRecoderStack:ListBuffer[LabelInfo] = new ListBuffer[LabelInfo] // stack is used for recode label information for "point" output
+    val nodeRecorder:ListBuffer[NodeInfo] = new ListBuffer[NodeInfo] // nodeRecorder is used for recode node information for "box" output
+    display(trie.root, 0, 0, labelRecoderStack, nodeRecorder)
     out.println("}")
   }
 
-  def display(root:TrieNode, level:Int = 0, nodeNumber:Int = 0, stack:ListBuffer[LabelInfo] = null):(Int,ListBuffer[LabelInfo]) = {
-    var newNodeNumber = nodeNumber
-    var newStack = stack
+  def display(root:TrieNode, level:Int = 0, nodeNumber:Int = 0, labelRecoderStack:ListBuffer[LabelInfo] = null,
+              nodeRecorder:ListBuffer[NodeInfo] = null):(Int,ListBuffer[LabelInfo]) = {
+    var newNodeNumber = nodeNumber // newNodeNumber is used to generate the number(ID) of the node for the "point" graph
+    var newLabelStack = labelRecoderStack
     if (root.isLeaf) {
-      if (newStack != null) {
-        for (i <- newStack.indices){
+      if (newLabelStack != null) {
+        // output "point" graph
+        for (i <- newLabelStack.indices){
           newNodeNumber = newNodeNumber + 1
-          if (i == 0) out.println(i + "->" + newNodeNumber + newStack(i).label)
-          else if (newStack(i).self){
+          if (i == 0) out.println(i + "->" + newNodeNumber + newLabelStack(i).label)
+          else if (newLabelStack(i).self){
             newNodeNumber = newNodeNumber -1
-            out.println(newNodeNumber + "->" + newNodeNumber + newStack(i).label)
+            out.println(newNodeNumber + "->" + newNodeNumber + newLabelStack(i).label)
           }
-          else out.println(newNodeNumber -1 + "->" + newNodeNumber + newStack(i).label)
+          else out.println(newNodeNumber -1 + "->" + newNodeNumber + newLabelStack(i).label)
         }
       }
-      if (newStack != null){
-        newStack.trimEnd(1)
+      if (newLabelStack != null){
+        newLabelStack.trimEnd(1)
       }
-      return (newNodeNumber, newStack)
+      return (newNodeNumber, newLabelStack)
     }
     for (t <- root.children.keySet) {
       val node = root.children.getOrElse(t,sys.error(s"unexpected key: $t"))
@@ -63,30 +67,60 @@ class PathVisualizer(trie:Trie, shape:String) {
       val transID = node.transitionInfo.transitionID.toString
       val transExecutionCounter = node.transitionInfo.transCounter.toString
       val selfTransCounter = "(Transition Self-execution Times:"+ node.selfTransCounter +")"
-      val newlabel =  "[label = \"" + "Model Name:" + modelName  + "\\n" +
+      // the newlabel is used for constructing a label for the output of the "point" graph
+      val newLabel =  "[label = \"" + "Model Name:" + modelName  + "\\n" +
         "Model ID:" + modelID + "\\n" +
         "Transition Name:" + transName  + "\\n" +
         "Transition ID:" + transID  + "\\n" +
         "Transition Execution Counter:" + transExecutionCounter  + "\\n" +
         selfTransCounter + "\"];"
       shape.toLowerCase match {
-        case "box" => val nodeIDFrom = transName.split(" => ")(0)
-                      val nodeIDTo = transName.split(" => ")(1).stripSuffix(" (1)")
-                      out.println("  " + nodeIDFrom + "->" + nodeIDTo  + newlabel)
-                       display(node,level+1)
-        case "point" =>
-                        val newlabelInfo = if (transName.split(" => ")(0) == transName.split(" => ")(1).stripSuffix(" (1)")) LabelInfo(newlabel,true)
-                                           else LabelInfo(newlabel)
-                        newStack += newlabelInfo
+        case "box" => //val nodeIDFrom = transName.split(" => ")(0)
+                      //val nodeIDTo = transName.split(" => ")(1).stripSuffix(" (1)")
+                      //out.println("  " + nodeIDFrom + "->" + nodeIDTo  + newlabel)
+                      var sameTransition = false
+                      if (nodeRecorder != null){
+                        for (n <- nodeRecorder){
+                          // check if the transition already in the nodeRecorder list
+                          if (n.node.transitionInfo.transitionName == node.transitionInfo.transitionName){
+                            sameTransition = true
+                            n.transCounter = n.transCounter.concat(";" + node.transitionInfo.transCounter.toString)
+                          }
+                        }
+                      }
+                      if (!sameTransition){
+                        val newNodeInfo = NodeInfo(node,transExecutionCounter)
+                        nodeRecorder += newNodeInfo
+                      }
+                      display(node,level+1,0,null,nodeRecorder)
+        case "point" => val newlabelInfo =
+                            if (transName.split(" => ")(0) == transName.split(" => ")(1).stripSuffix(" (1)"))
+                              LabelInfo(newLabel,true)
+                            else LabelInfo(newLabel)
+                        newLabelStack += newlabelInfo
                        // Log.info("new stack:" + newStack)
-                        val result = display(node,level+1, newNodeNumber, newStack)
+                        val result = display(node,level+1, newNodeNumber, newLabelStack)
                         newNodeNumber = result._1
-                        newStack = result._2
+                        newLabelStack = result._2
       }
     }
-    if (newStack != null){
-      newStack.trimEnd(1)
+    if (newLabelStack != null){
+      newLabelStack.trimEnd(1)
     }
-    (newNodeNumber, newStack)
+    // output "box" graph
+    if (level == 0 && nodeRecorder != null && nodeRecorder.nonEmpty) {
+      //Log.info(nodeRecorder.toString())
+      for (n <- nodeRecorder) {
+        out.println("  " + n.node.transitionInfo.transitionName.split(" => ")(0)
+          + "->" + n.node.transitionInfo.transitionName.split(" => ")(1).stripSuffix(" (1)")
+          + "[label = \"" + "Model Name:" + n.node.modelInfo.modelName  + "\\n" +
+          "Model ID:" + n.node.modelInfo.modelID.toString + "\\n" +
+          "Transition Name:" + n.node.transitionInfo.transitionName.stripSuffix(" (1)")  + "\\n" +
+          "Transition ID:" + n.node.transitionInfo.transitionID.toString  + "\\n" +
+          "Transition Execution Counter:" + n.transCounter  + "\\n" +
+          "(Transition Self-execution Times:"+ n.node.selfTransCounter +")" + "\"];")
+      }
+    }
+    (newNodeNumber, newLabelStack)
   }
 }
