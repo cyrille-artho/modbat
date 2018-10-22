73975c865c9565f4f052ba9658e47f6fbb1d23f1
Rui Wang <wruiwr86@gmail.com>
2018-10-22 10:56:02 +0200

    reorganize the code and fix a bug

--- src/main/scala/modbat/cov/Trie.scala
+++ src/main/scala/modbat/cov/Trie.scala
@@ -4,32 +4,45 @@ import modbat.dsl.{State, Transition}
 import modbat.log.Log
 import modbat.mbt.PathInfo
 import modbat.mbt.TransitionQuality.Quality
-
 import scala.collection.mutable.{HashMap, ListBuffer}
 
+/** Trie stores the path information for the path coverage. */
 class Trie {
   val root: TrieNode = TrieNode()
   // The insert method inserts the transition of each test into a trie data structure
-  def insert(pathInfo:ListBuffer[PathInfo]) = {
+  def insert(pathInfo: ListBuffer[PathInfo]): Unit = {
     var currentNode: TrieNode = root
+
     for (p <- pathInfo) {
-      var node:TrieNode = currentNode.children.getOrElse(p.transition.idx,null)
-      if (currentNode.currentTransition != null && currentNode.currentTransition == p.transition) {
+      // check and get node in trie
+      var node: TrieNode =
+        currentNode.children.getOrElse(p.transition.idx, null)
+      // check if the new transition is a self loop in the path
+      if (currentNode.currentTransition != null && currentNode.currentTransition.idx == p.transition.idx) {
         currentNode.selfTransCounter += 1
-       Log.info("the transition stored in trie node:" + p.transition.toString() + " counter:" + currentNode.selfTransCounter)
-      }else {
+        Log.info(
+          "the transition stored in trie node:" + p.transition.toString +
+            " counter:" + currentNode.selfTransCounter)
+      } else {
         if (node == null) {
+          // new node
           node = TrieNode()
           node.currentTransition = p.transition
-          node.modelInfo = ModelInfo(p.modelName,p.modelID)
-          node.transitionInfo = TransitionInfo(p.transition.origin, p.transition.dest, /*p.transition.toString(),*/ p.transition.idx,1,p.transitionQuality)
+          node.modelInfo = ModelInfo(p.modelName, p.modelID)
+          node.transitionInfo = TransitionInfo(p.transition.origin,
+                                               p.transition.dest,
+                                               p.transition.idx,
+                                               1,
+                                               p.transitionQuality)
           currentNode.children.put(node.transitionInfo.transitionID, node)
-        }else{
+        } else {
+          // existing node
           Log.info("transition already exist in trie:" + node.transitionInfo)
           if (node.transitionInfo.transitionID == p.transition.idx) {
             node.transitionInfo.transCounter = node.transitionInfo.transCounter + 1
-           // node.transitionInfo = node.transitionInfo.copy(_3 = node.transitionInfo._3 + 1)
-            Log.info ("got a same transition executed: " + node.transitionInfo.transCounter + " times")
+            // node.transitionInfo = node.transitionInfo.copy(_3 = node.transitionInfo._3 + 1)
+            Log.info(
+              "got a same transition executed: " + node.transitionInfo.transCounter + " times")
           }
         }
         currentNode = node
@@ -38,39 +51,71 @@ class Trie {
     currentNode.isLeaf = true
   }
 
-  def display(root:TrieNode, level:Int = 0):Unit = {
+  def display(root: TrieNode, level: Int = 0): Unit = {
     if (root.isLeaf) println()
 
     for (t <- root.children.keySet) {
-      val node = root.children.getOrElse(t,sys.error(s"unexpected key: $t"))
+      val node: TrieNode =
+        root.children.getOrElse(t, sys.error(s"unexpected key: $t"))
       if (level == 0) {
-        Log.info(node.modelInfo.toString() + node.transitionInfo.toString() + " ("+ node.selfTransCounter +")")
-      }
-      else Log.info("-"*level + node.modelInfo.toString() + node.transitionInfo.toString() + " ("+ node.selfTransCounter +")")
-      display(node,level+1)
+        Log.info(
+          node.modelInfo.toString + node.transitionInfo.toString + " (" + node.selfTransCounter + ")")
+      } else
+        Log.info(
+          "-" * level + node.modelInfo.toString + node.transitionInfo.toString +
+            " (" + node.selfTransCounter + ")")
+      display(node, level + 1)
     }
   }
   // The method numOfPaths computes the total number of paths
-  def numOfPaths(root:TrieNode):Int = {
+  def numOfPaths(root: TrieNode): Int = {
     var result = 0
 
     if (root.isLeaf) result += 1
     for (t <- root.children.keySet) {
-      val node = root.children.getOrElse(t,sys.error(s"unexpected key: $t"))
+      val node = root.children.getOrElse(t, sys.error(s"unexpected key: $t"))
       result += numOfPaths(node)
     }
     result
   }
 }
 
+/** rieNode stores each current transition's information
+  * and next node in the trie.
+  */
 case class TrieNode() {
-  var children: HashMap[Int,TrieNode] = HashMap.empty[Int,TrieNode] // children store the transitions in string and the next nodes
+  var children
+    : HashMap[Int, TrieNode] = HashMap.empty[Int, TrieNode] // children store the transitions in string and the next nodes
   var isLeaf: Boolean = false
   var selfTransCounter = 1 // this counter counts the number of times for a transition occurring to the same state itself during a test
-  var currentTransition:Transition = null  // previousTransition stores the transition leading to the next state
-  var modelInfo: ModelInfo  = _ // modelInfo stores the name of the model and its id
-  var transitionInfo: TransitionInfo  = _ // transitionInfo store the transition as a string, its id, and the times that transition are executed
+  var currentTransition
+    : Transition = null // previousTransition stores the transition leading to the next state
+  var modelInfo
+    : ModelInfo = _ // modelInfo stores the name of the model and its id
+  var transitionInfo
+    : TransitionInfo = _ // transitionInfo store the transition as a string, its id, and the times that transition are executed
 }
 
-case class ModelInfo(modelName:String, modelID:Int)
-case class TransitionInfo(transOrigin:State, transDest:State, /*transitionName:String,*/ transitionID:Int, var transCounter:Int, transitionQuality:Quality)
+/** ModelInfo stores a model's information.
+  *
+  * @constructor create a new modelInfo with a modelName and modelID.
+  * @param modelName the model's name
+  * @param modelID the model's ID
+  */
+case class ModelInfo(modelName: String, modelID: Int)
+
+/** TransitionInfo stores a transition's information.
+  *
+  * @constructor create a new transitionInfo with a transOrigin, transDest,
+  *              transitionID, transCounter, and transitionQuality.
+  * @param transOrigin the origin state of the transition
+  * @param transDest the target state of the transition
+  * @param transitionID the transition's ID
+  * @param transCounter the number of time that transition is executed in a path
+  * @param transitionQuality the quality of the transition, which could be OK, Backtrack, or Fail
+  */
+case class TransitionInfo(transOrigin: State,
+                          transDest: State,
+                          transitionID: Int,
+                          var transCounter: Int,
+                          transitionQuality: Quality)
---
+++ src/main/scala/modbat/mbt/PathInPoint.scala
@@ -0,0 +1,98 @@
+package modbat.mbt
+
+import modbat.cov.{Trie, TrieNode}
+import modbat.dsl.State
+import scala.collection.mutable.ListBuffer
+
+case class LabelInfo(label: String, self: Boolean = false) // LabelInfo is used for record the label information used for "point" output
+
+class PathInPoint(trie: Trie, val shape: String) extends PathVisualizer {
+
+  override def dotify() {
+    out.println("digraph model {")
+    out.println("  orientation = landscape;")
+    out.println(
+      "  graph [ rankdir = \"TB\", ranksep=\"0.3\", nodesep=\"0.2\" ];")
+    out.println(
+      "  node [ fontname = \"Helvetica\", fontsize=\"6.0\", shape=\"" + shape.toLowerCase +
+        "\", margin=\"0.07\"," + " height=\"0.1\" ];")
+    out.println(
+      "  edge [ fontname = \"Helvetica\", fontsize=\"6.0\"," + " margin=\"0.05\" ];")
+    val labelRecoderStack
+      : ListBuffer[LabelInfo] = new ListBuffer[LabelInfo] // stack is used for recode label information for "point" output
+    display(trie.root, 0, 0, labelRecoderStack)
+    out.println("}")
+  }
+
+  def display(root: TrieNode,
+              level: Int = 0,
+              nodeNumber: Int = 0,
+              labelRecoderStack: ListBuffer[LabelInfo] = null)
+    : (Int, ListBuffer[LabelInfo]) = {
+
+    var newNodeNumber
+      : Int = nodeNumber // newNodeNumber is used to generate the number(ID) of the node for the "point" graph
+    var newLabelStack: ListBuffer[LabelInfo] = labelRecoderStack
+
+    if (root.isLeaf) {
+      if (newLabelStack != null) {
+        // output "point" graph
+        for (i <- newLabelStack.indices) {
+          newNodeNumber = newNodeNumber + 1
+          if (i == 0)
+            out.println(i + "->" + newNodeNumber + newLabelStack(i).label)
+          else if (newLabelStack(i).self) {
+            newNodeNumber = newNodeNumber - 1
+            out.println(
+              newNodeNumber + "->" + newNodeNumber + newLabelStack(i).label)
+          } else
+            out.println(
+              newNodeNumber - 1 + "->" + newNodeNumber + newLabelStack(i).label)
+        }
+      }
+      if (newLabelStack != null) {
+        newLabelStack.trimEnd(1)
+      }
+      return (newNodeNumber, newLabelStack)
+    }
+    for (t <- root.children.keySet) {
+      val node: TrieNode =
+        root.children.getOrElse(t, sys.error(s"unexpected key: $t"))
+      val modelName: String = node.modelInfo.modelName
+      val modelID = node.modelInfo.modelID.toString
+      val transID = node.transitionInfo.transitionID.toString
+      val transOriginState: State = node.transitionInfo.transOrigin
+      val transDestState: State = node.transitionInfo.transDest
+      val transName = transOriginState.toString + " => " + transDestState.toString
+      val transQuality: TransitionQuality.Quality =
+        node.transitionInfo.transitionQuality
+      val transExecutionCounter = node.transitionInfo.transCounter.toString
+      val selfTransCounter = "(T-Self:" + node.selfTransCounter + ")"
+      val edgeStyle: String =
+        if (transQuality == TransitionQuality.backtrack)
+          "style=dotted, color=red,"
+        else ""
+      // the newlabel here is used for constructing a label for the output of the "point" graph
+      val newLabel = "[" + edgeStyle + "label = \"" + "M:" + modelName + "\\n" +
+        "M-ID:" + modelID + "\\n" +
+        "T:" + transName + "\\n" +
+        "T-ID:" + transID + "\\n" +
+        "T-Counter:" + transExecutionCounter + "\\n" +
+        selfTransCounter + "\"];"
+      val newlabelInfo =
+        if (transOriginState == transDestState)
+          LabelInfo(newLabel, true)
+        else LabelInfo(newLabel)
+      newLabelStack += newlabelInfo
+      // Log.info("new stack:" + newStack)
+      val result = display(node, level + 1, newNodeNumber, newLabelStack)
+      newNodeNumber = result._1
+      newLabelStack = result._2
+    }
+    if (newLabelStack != null) {
+      newLabelStack.trimEnd(1)
+    }
+    (newNodeNumber, newLabelStack)
+  }
+
+}
