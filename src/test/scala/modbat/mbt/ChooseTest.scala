2d918d25ee10409c392b4f2d72086e6fa86728b7
Alexandre Fournel <alexandrefournel@n151-p147.eduroam.kth.se>
2019-05-22 14:55:18 +0200

    Issue #54: ModbatTestHarness.scala.setTestJar does set CLASSPATH correctly, now

--- src/test/scala/modbat/mbt/ChooseTest.scala
+++ src/test/scala/modbat/mbt/ChooseTest.scala
@@ -4,7 +4,7 @@ import org.scalatest._
 
 class ChooseTest extends FlatSpec with Matchers with AppendedClues {
   "Choose00" should "pass with one transition" in {
-    val result = ModbatTestHarness.testMain(Array("-s=1", "-n=1", "--no-redirect-out", "--classpath=build/modbat-test.jar", "-s", "modbat.test.Choose00"), ModbatTestHarness.setTestJar)
+    val result = ModbatTestHarness.testMain(Array("-s=1", "-n=1", "--no-redirect-out", "-s", "modbat.test.Choose00"), ModbatTestHarness.setTestJar)
     result._1 should be(0)
     result._2.filter(_.contains("1 tests executed, 1 ok, 0 failed.")) should not be empty withClue(result._2.mkString("\n"))
     result._2.filter(_.contains("2 states covered (100 % out of 2),")) should not be empty withClue(result._2.mkString("\n"))
--- src/test/scala/modbat/mbt/TimeTest.scala
+++ src/test/scala/modbat/mbt/TimeTest.scala
@@ -4,7 +4,7 @@ import org.scalatest._
 
 class TimeTest extends FlatSpec with Matchers {
   "Time1" should "pass with one transition" in {
-    val result = ModbatTestHarness.testMain(Array("-s=1", "-n=1", "--no-redirect-out", "--classpath=build/modbat-test.jar", "-s",  "modbat.test.Time1"), ModbatTestHarness.setTestJar)
+    val result = ModbatTestHarness.testMain(Array("-s=1", "-n=1", "--no-redirect-out", "-s",  "modbat.test.Time1"), ModbatTestHarness.setTestJar)
     result._1 should be(0)
     result._2.filter(_.contains("1 tests executed, 1 ok, 0 failed.")) should not be empty
     result._2.filter(_.contains("2 states covered (100 % out of 2),")) should not be empty
@@ -13,7 +13,7 @@ class TimeTest extends FlatSpec with Matchers {
   }
 
   "Time2" should "pass with three transitions" in {
-    val result = ModbatTestHarness.testMain(Array("-s=1", "-n=1", "--no-redirect-out", "--classpath=build/modbat-test.jar", "-s",  "modbat.test.Time2"), ModbatTestHarness.setTestJar)
+    val result = ModbatTestHarness.testMain(Array("-s=1", "-n=1", "--no-redirect-out", "-s",  "modbat.test.Time2"), ModbatTestHarness.setTestJar)
     //for(str <- result._2) println(str)
     result._1 should be(0)
     result._2.filter(_.contains("1 tests executed, 1 ok, 0 failed.")) should not be empty
