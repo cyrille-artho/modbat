82d04ae28f1d37e145f9c6d6aa8ecc1b8c30eed2
Alexandre Fournel <alexandrefournel@Alex-Network.local>
2019-06-25 15:51:10 +0200

    Issue #55:Big refactoring in ModbatTestHarness. Now the comparison between former and new files works. I changed also the function replaceRegexInSentence using Maps. To conclue, for the err files, the grep -v removing the all sentence if regex matches has to be implement. At that time, we replace the sentence by an empty line... But does it impact the result of the comparison, I'm not sure...

--- src/test/scala/modbat/mbt/ModbatTestHarness.scala
+++ src/test/scala/modbat/mbt/ModbatTestHarness.scala
@@ -20,12 +20,38 @@ import modbat.log.Log
 import modbat.util.CloneableRandom
 
 object ModbatTestHarness {
+  def getNamesofFile(args: Array[String], td: org.scalatest.TestData): (String, String, String) = {
+    // Be careful, this function create folder if doesn't exist
+    var name_output = "log/modbat/"
+    var arguments_name = ""
+    for ( x <- args ) {
+      if (x contains "modbat"){
+        name_output = name_output + x
+      }
+      else{
+        arguments_name = arguments_name + x
+      }
+    }
+    var directory = new File(name_output);
+    var bool = ! directory.exists()
+    if (bool){
+        directory.mkdirs();
+    }
+    (name_output, arguments_name, ( td.name.split(" ") )(0))
+  }
 
-  def using[A <: {def close(): Unit}, B](resource: A)(f: A => B): B =
-  try f(resource) finally resource.close()
-
-  def writeToFile(path: String, data: String): Unit = 
-    using(new FileWriter(path))(_.write(data))
+  def addToFiles(writer_1: java.io.PrintWriter,writer_2: java.io.PrintWriter,data:String){
+    writer_1.write(data+"\n")
+    writer_2.write(data+"\n")  
+  }
+  
+  def write(output_1: String, output_2: String, iterator_data: Iterator[String]): Unit = {
+    val (writer_1, writer_2) = (new PrintWriter(new File(output_1)), new PrintWriter(new File(output_2)))
+    while (iterator_data.hasNext) 
+      addToFiles(writer_1, writer_2, iterator_data.next())
+    writer_1.close()
+    writer_2.close()
+  }
 
   def savemv(from : String, to : String) {
     val src = new File(from)
@@ -35,8 +61,7 @@ object ModbatTestHarness {
   }
 
   def compare_iterators(it1:Iterator[String], it2:Iterator[String]) : Boolean = {
-    var current_iterator="There is no element in the Iterator."
-    var current_iterator2="There is no element in the Iterator."
+    var current_iterator, current_iterator2="There is no element in the Iterator."
     var current_comparison = true
     while (current_comparison && (it1.hasNext || it2.hasNext)){
       ((it1.hasNext, it2.hasNext) match{
@@ -45,7 +70,7 @@ object ModbatTestHarness {
           current_iterator2 = it2.next
           if (current_iterator != current_iterator2){
             current_comparison=false
-            current_iterator="iterators different : \n validated_out_lines = " + current_iterator + "\n out = " + current_iterator2
+            current_iterator="iterators different : \n validated_out_lines = \n" + current_iterator + "\n out_filtered = \n" + current_iterator2
           }
         }
         case (false, false) => {
@@ -71,45 +96,36 @@ object ModbatTestHarness {
     current_comparison
   }
 
-  def filtering_and_regex(name_output : String, typeoutput : String, typeByte : ByteArrayOutputStream, name_file: String){
-    var regex_list = List("")
+  def filtering_writing_comparing_each_file(name_output_1 : String, name_output_2 : String, typeByte : ByteArrayOutputStream){
+    var regex_map = Map[String, String]()
     var replace_sentences = List("")
-    if (typeoutput == "out"){
-      regex_list = List("""\[[0-9][0-9]*[mK]""", """.*""", """ in .*[0-9]* milliseconds""", """RELEASE-([0-9.]*)""",  """ v[0-9a-f]* rev [0-9a-f]*""", """ v[0-9][0-9]*\\.[0-9][^ ]* rev [0-9a-f]*""", """^Time: [0-9.]*""", """(at .*):[0-9]*:""", """canceled 0, """, """(\[INFO\] .* java.lang.AssertionError.*):""")
-      replace_sentences=List("", "", "", "$1", "", " vx.yz", " vx.yz", "$1", "", "$1")
+    if (name_output_1.split("\\.").last == "out"){
+      regex_map = Map("""\[[0-9][0-9]*[mK]"""->"",""".*"""->"",""" in .*[0-9]* milliseconds"""->"","""RELEASE-([0-9.]*)"""->"$1",""" v[0-9a-f]* rev [0-9a-f]*"""->"",""" v[0-9][0-9]*\\.[0-9][^ ]* rev [0-9a-f]*"""->" vx.yz","""^Time: [0-9.]*"""->" vx.yz","""(at .*):[0-9]*:"""->"$1","""canceled 0, """->"","""(\[INFO\] .* java.lang.AssertionError.*):"""->"$1")
     }
-    else{
-      regex_list = List("""RELEASE-3.2""", """ v[0-9a-f]* rev [0-9a-f]*""", """ v[^ ]* rev [0-9a-f]*""", """(at .*?):[0-9]*:?""", """(Exception in thread "Thread-)[0-9][0-9]*""", """CommonRunner.*un.*\(ObjectRunner.scala""", """MainGenericRunner.*un.*\(MainGenericRunner.scala""")
-      replace_sentences = List("3.3", " vx.yz", " vx.yz", "$1", "$1", "", "")
+    else if (name_output_1.split("\\.").last == "eout"){
+      regex_map = Map ("""RELEASE-3.2"""->"3.3", """ v[0-9a-f]* rev [0-9a-f]*"""->" vx.yz", """ v[^ ]* rev [0-9a-f]*"""->" vx.yz", """(at .*?):[0-9]*:?"""->"$1", """(Exception in thread "Thread-)[0-9][0-9]*"""->"$1", """CommonRunner.*un.*\(ObjectRunner.scala"""->"", """MainGenericRunner.*un.*\(MainGenericRunner.scala"""->"")
     }
-    val validated_out=name_output
+    else Console.println("Error : ModbatTestHarness - Unexpected end of file name") 
+    val validated_out=name_output_1
     if (new java.io.File(validated_out).exists){
-      val validated_out_lines = Source.fromFile(name_output).getLines
+      val validated_out_lines = Source.fromFile(name_output_1).getLines
       val out_lines = Source.fromBytes(typeByte.toByteArray()).getLines
-
-      // list of regex to remove
-      //val regex1 = """\[FINE\].*""".r
-      
-      // val it2 = out_lines.filterNot(x => x match { case regex1() => true case _ => false})
-
-      val filewhichcanbecompared = (/*it2*/out_lines.map (l => replaceRegexInSentence(l, regex_list, replace_sentences)))
-      assert(compare_iterators(validated_out_lines, filewhichcanbecompared))
+      val (filewhichcanbecompared,filewhichcanbecompared_copy) = out_lines.map (l => replaceRegexInSentence(l, regex_map)).duplicate
+      write(name_output_1,name_output_2, filewhichcanbecompared)
+      assert(compare_iterators(validated_out_lines, filewhichcanbecompared_copy))
     }
   }
 
-  def replaceRegexInSentence(sentence : String, list_pattern : List[String], replace_sentences : List[String]) : String = {  
-    if (list_pattern.length !=0){
-      val pattern = list_pattern(0)
-      val replace_sentence = replace_sentences(0)
-      replaceRegexInSentence(
-        sentence.replaceAll(pattern, replace_sentence), 
-        list_pattern.drop(1), 
-        replace_sentences.drop(1)
-      )
-    }
-    else{
-      sentence
-    }
+  def filtering_writing_comparing(name_output: String, arguments_name: String, simple_name:String, out : ByteArrayOutputStream, err : ByteArrayOutputStream){
+    filtering_writing_comparing_each_file (name_output+arguments_name+".out", name_output+simple_name+".out", out)
+    filtering_writing_comparing_each_file (name_output+arguments_name+".eout", name_output+simple_name+".eout", err)
+  }
+
+  def replaceRegexInSentence(sentence : String, regex_map : Map[String,String]) : String = {  
+    var sentence_filtred = sentence
+    regex_map foreach (x => 
+      sentence_filtred = sentence_filtred.replaceAll(x._1, x._2))
+    sentence_filtred
   }
 
   def testMain(args: Array[String], env: () => Unit, td: org.scalatest.TestData, optionsavemv : Option [(String, String)] = None): (Int, List[String], List[String]) = {
@@ -134,44 +150,10 @@ object ModbatTestHarness {
         }
       }
     }
-    var name_output = "log/modbat/"
-    var name_file = ""
-    for ( x <- args ) {
-      if (x contains "modbat"){
-        name_output = name_output + x
-      }
-      else{
-        name_file = name_file + x
-      }
-    }
-    var directory = new File(name_output);
-    var bool = ! directory.exists()
-    if (bool){
-        directory.mkdirs();
-    }
 
-    val simple_name = ( td.name.split(" ") )(0)
-    val name_output_1=name_output+"/"+name_file
-    val name_output_2=name_output+"/"+simple_name
+    val (name_output, arguments_name, simple_name) = getNamesofFile(args, td)
     
-
-    val name_output_err_1=name_output_1+".err"
-    val name_output_out_1=name_output_1+".log"
-
-    val name_output_err_2=name_output_2+".err"
-    val name_output_out_2=name_output_2+".log"
-      
-    val err_value = err.toString
-    val eout_value = out.toString
-  
-    writeToFile(name_output_err_1, err_value)
-    writeToFile(name_output_out_1, eout_value)
-    
-    writeToFile(name_output_err_2, err_value)
-    writeToFile(name_output_out_2, eout_value)
-
-    filtering_and_regex (name_output_1+".out", "out", out, simple_name)
-    /* filtering_and_regex (name_output_1+".eout", "err", err, simple_name) */
+    filtering_writing_comparing (name_output+"/", arguments_name, simple_name, out, err)
     
     optionsavemv match {
       case None => {}
@@ -180,7 +162,7 @@ object ModbatTestHarness {
       }
     }
 
-    (ret, scala.io.Source.fromString(eout_value).getLines().toList, scala.io.Source.fromString(err_value).getLines().toList)
+    (ret, scala.io.Source.fromString(out.toString).getLines().toList, scala.io.Source.fromString(err.toString).getLines().toList)
   }
 
   def setEnv(newEnv: java.util.Map[String, String]) {
@@ -214,23 +196,22 @@ object ModbatTestHarness {
     }
   }
 
-  def setTestJar() = {
+  def setJar(arg:String)={
     val mapsetTestJar = new java.util.HashMap[String, String]()
-    mapsetTestJar.put("CLASSPATH", "build/modbat-test.jar")
+    mapsetTestJar.put("CLASSPATH", arg)
     setEnv (mapsetTestJar)
+  }
+
+  def setTestJar() = {
+    setJar("build/modbat-test.jar")
   }
 
   def setExamplesJar() = {
-    val mapsetTestJar = new java.util.HashMap[String, String]()
-    mapsetTestJar.put("CLASSPATH", "build/modbat-examples.jar")
-    setEnv (mapsetTestJar)
+    setJar("build/modbat-examples.jar")
   }
 
   def setFooJar() = {
-    val mapsetTestJar = new java.util.HashMap[String, String]()
-    mapsetTestJar.put("CLASSPATH", "foo")
-    setEnv (mapsetTestJar)
+    setJar("foo")
   }
 
 }
-
