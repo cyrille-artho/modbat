812e90a11847fd25a086852121af9deb7785d0a5
Cyrille Artho <artho@kth.se>
2019-06-26 11:26:24 +0200

    Fix: Removed extra argument that made tests fail. 	modified:   src/test/scala/modbat/mbt/ConfigTest.scala 	modified:   src/test/scala/modbat/mbt/ModbatTestHarness.scala: Added EOL at EOF.

--- src/test/scala/modbat/mbt/ConfigTest.scala
+++ src/test/scala/modbat/mbt/ConfigTest.scala
@@ -4,13 +4,13 @@ import org.scalatest._
 
 class ConfigTest extends fixture.FlatSpec with fixture.TestDataFixture with Matchers {
   "ConfigTest1" should "pass" in { td =>
-    val result = modbat.config.ConfigTest.main(Array("-cp","modbat.config.ConfigTest","-h"))
+    val result = modbat.config.ConfigTest.main(Array("modbat.config.ConfigTest","-h"))
     result._1 should be(0)
     result._3 shouldBe empty
   }
 
   "ConfigTest2" should "pass" in { td =>
-    val result = modbat.config.ConfigTest.main(Array("-cp","modbat.config.ConfigTest","-s"))
+    val result = modbat.config.ConfigTest.main(Array("modbat.config.ConfigTest","-s"))
     result._1 should be(0)
     result._3 shouldBe empty
   }
