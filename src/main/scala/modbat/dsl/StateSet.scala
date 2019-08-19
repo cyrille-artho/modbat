6bdcc33f1b0efd4e636f62cca4a329b370d7a4ed
Cyrille Artho <artho@kth.se>
2019-08-19 11:52:47 +0900

    Fixed error trace for case where list of pre-states maps to post-state with regular trans. 	modified:   src/main/scala/modbat/dsl/StateSet.scala 	modified:   log/modbat/modbat.test.StateSetToTrans/-s=1-n=1--no-redirect-out.out

--- src/main/scala/modbat/dsl/StateSet.scala
+++ src/main/scala/modbat/dsl/StateSet.scala
@@ -5,12 +5,14 @@ import scala.collection.mutable.ListBuffer
 class StateSet(preStates: List[String], postState: String) {
   val dest = new State(postState)
 
-  def := (action: => Any): List[Transition] = {
+  def := (action: => Any)
+    (implicit line: sourcecode.Line, fullName: sourcecode.FullName):
+    List[Transition] = {
     val trans = new ListBuffer[Transition]
     val a = new Action(() => action)
     for (origin <- preStates) {
       trans +=
-	new Transition(new State(origin), dest, false, a, "???", -1)
+	new Transition(new State(origin), dest, false, a, fullName.value, line.value)
     }
     trans.toList
   }
