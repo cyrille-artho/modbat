8cb7ba67b7e0b69d67cceca9d463781d3ce8f2aa
Cyrille Artho <artho@kth.se>
2019-08-24 13:38:24 +0900

    modified:   src/test/scala/modbat/mbt/EmptyModel.scala: Marked failure-inducing input as such.

--- src/test/scala/modbat/mbt/EmptyModel.scala
+++ src/test/scala/modbat/mbt/EmptyModel.scala
@@ -4,7 +4,7 @@ import org.scalatest._
 
 class EmptyModel extends fixture.FlatSpec with fixture.TestDataFixture with Matchers {
   "EmptyModel1" should "fail" in { td =>
-    val result = ModbatTestHarness.testMain(Array("-s=1","--no-redirect-out","modbat.test.EmptyModel"), ModbatTestHarness.setTestJar, td)
+    val result = ModbatTestHarness.testMain(Array("-s=1","--no-redirect-out","modbat.test.EmptyModel"), ModbatTestHarness.setTestJar, td, true)
     result._1 should be(1)
     result._3 should not be empty
   }
