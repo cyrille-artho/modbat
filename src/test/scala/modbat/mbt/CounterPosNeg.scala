6c7722d218f5b64c3c0667e465cbbcabfab6d82f
Cyrille Artho <artho@kth.se>
2019-08-26 16:14:48 +0900

    modified:   src/test/scala/modbat/mbt/CounterPosNeg.scala: Converted another test from bin/test.sh.

--- src/test/scala/modbat/mbt/CounterPosNeg.scala
+++ src/test/scala/modbat/mbt/CounterPosNeg.scala
@@ -7,4 +7,9 @@ class CounterPosNeg extends fixture.FlatSpec with fixture.TestDataFixture with M
     val result = ModbatTestHarness.testMain(Array("--mode=dot","--auto-labels","modbat.examples.CounterPosNeg"), ModbatTestHarness.setExamplesJar, td, Some("modbat.examples.CounterPosNeg.dot","counterpn.dot"))
     result._2 shouldBe empty
   }
+
+  "CounterPosNeg2" should "pass" in { td =>
+    val result = ModbatTestHarness.testMain(Array("--mode=dot","--auto-labels","modbat.test.CounterPosNeg1"), ModbatTestHarness.setTestJar, td, Some("modbat.test.CounterPosNeg1.dot","counterpn1.dot"))
+    result._2 shouldBe empty
+  }
 }
