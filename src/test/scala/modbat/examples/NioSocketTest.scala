24f0f0170d71b4a8a54ca61b6d2cf1fc05645fcc
Cyrille Artho <artho@kth.se>
2020-08-10 13:00:52 +0200

    Updated Modbat unit test harness to use test data fixture. ConfigTestHarness still uses an explicit return value parameter. 	modified:   src/test/scala/modbat/examples/NioSocketTest.scala 	modified:   src/test/scala/modbat/mbt/ModbatTest.scala 	modified:   src/test/scala/modbat/mbt/ModbatTestHarness.scala

--- src/test/scala/modbat/examples/NioSocketTest.scala
+++ src/test/scala/modbat/examples/NioSocketTest.scala
@@ -3,20 +3,20 @@ package modbat.examples
 import org.scalatest._
 import modbat.mbt.ModbatTestHarness
 
-class NioSocketTest extends FlatSpec with Matchers {
-  "NioSocket1Test1" should "pass" in {
-    ModbatTestHarness.test(Array("-n=5", "-s=1", "--no-redirect-out", "--log-level=fine", "--no-init", "--no-shutdown", "modbat.examples.NioSocket1"), (() => ModbatTestHarness.setExamplesJar()))
+class NioSocketTest extends fixture.FlatSpec with fixture.TestDataFixture with Matchers {
+  "NioSocket1Test1" should "pass" in { td =>
+    ModbatTestHarness.test(Array("-n=5", "-s=1", "--no-redirect-out", "--log-level=fine", "--no-init", "--no-shutdown", "modbat.examples.NioSocket1"), (() => ModbatTestHarness.setExamplesJar()), td)
   }
 
-  "NioSocket1Test2" should "pass" in {
-    ModbatTestHarness.test(Array("-n=10", "-s=1", "--no-redirect-out", "--log-level=fine", "--no-init", "--no-shutdown", "modbat.examples.NioSocket1"), (() => ModbatTestHarness.setExamplesJar()))
+  "NioSocket1Test2" should "pass" in { td =>
+    ModbatTestHarness.test(Array("-n=10", "-s=1", "--no-redirect-out", "--log-level=fine", "--no-init", "--no-shutdown", "modbat.examples.NioSocket1"), (() => ModbatTestHarness.setExamplesJar()), td)
   }
 
-  "NioSocket1Test3" should "pass" in {
-    ModbatTestHarness.test(Array("-n=20", "-s=1", "--no-redirect-out", "--log-level=fine", "--no-init", "--no-shutdown", "modbat.examples.NioSocket1"), (() => ModbatTestHarness.setExamplesJar()))
+  "NioSocket1Test3" should "pass" in { td =>
+    ModbatTestHarness.test(Array("-n=20", "-s=1", "--no-redirect-out", "--log-level=fine", "--no-init", "--no-shutdown", "modbat.examples.NioSocket1"), (() => ModbatTestHarness.setExamplesJar()), td)
   }
 
-  "NioSocket1Test4" should "pass" in {
-    ModbatTestHarness.test(Array("-n=50", "-s=1", "--no-redirect-out", "--log-level=fine", "--no-init", "--no-shutdown", "modbat.examples.NioSocket1"), (() => ModbatTestHarness.setExamplesJar()))
+  "NioSocket1Test4" should "pass" in { td =>
+    ModbatTestHarness.test(Array("-n=50", "-s=1", "--no-redirect-out", "--log-level=fine", "--no-init", "--no-shutdown", "modbat.examples.NioSocket1"), (() => ModbatTestHarness.setExamplesJar()), td)
   }
 }
