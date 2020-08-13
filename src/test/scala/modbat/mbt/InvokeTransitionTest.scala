782f5fce7544e1eb49038e4d260fc2bfb44bded7
Cyrille Artho <artho@kth.se>
2020-08-13 09:37:54 +0200

    Converted tests to newer framework, as working with incorrect programmatic output checks is too cumbersome. 	modified:   src/test/scala/modbat/mbt/InvokeTransitionTest.scala

--- src/test/scala/modbat/mbt/InvokeTransitionTest.scala
+++ src/test/scala/modbat/mbt/InvokeTransitionTest.scala
@@ -2,41 +2,21 @@ package modbat.mbt
 
 import org.scalatest._
 
-class InvokeTransitionTest extends FlatSpec with Matchers {
-  "InvokeTransition1" should "pass with one transition" in {
-    val result = ModbatTestHarness.testMain(Array("-s=1", "-n=1", "--no-redirect-out", "-s",  "modbat.test.InvokeTransition1"), (() => ModbatTestHarness.setTestJar()))
-    result._1 should be(0)
-    result._2.filter(_.contains("1 tests executed, 1 ok, 0 failed.")) should not be empty
-    result._2.filter(_.contains("2 states covered (100 % out of 2),")) should not be empty
-    result._2.filter(_.contains("2 transitions covered (100 % out of 2).")) should not be empty
-    result._3 shouldBe empty
+class InvokeTransitionTest extends fixture.FlatSpec with fixture.TestDataFixture with Matchers {
+  "InvokeTransition1" should "pass with one transition" in { td =>
+    val result = ModbatTestHarness.test(Array("-s=1", "-n=1", "--no-redirect-out", "-s",  "modbat.test.InvokeTransition1"), (() => ModbatTestHarness.setTestJar()), td)
   }
 
-  "InvokeTransition2" should "pass with three transitions" in {
-    val result = ModbatTestHarness.testMain(Array("-s=1", "-n=1", "--no-redirect-out", "-s",  "modbat.test.InvokeTransition2"), (() => ModbatTestHarness.setTestJar()))
-    result._1 should be(0)
-    result._2.filter(_.contains("1 tests executed, 1 ok, 0 failed.")) should not be empty
-    result._2.filter(_.contains("3 states covered (100 % out of 3),")) should not be empty
-    result._2.filter(_.contains("3 transitions covered (100 % out of 3).")) should not be empty
-    result._3 shouldBe empty
+  "InvokeTransition2" should "pass with three transitions" in { td =>
+    val result = ModbatTestHarness.test(Array("-s=1", "-n=1", "--no-redirect-out", "-s",  "modbat.test.InvokeTransition2"), (() => ModbatTestHarness.setTestJar()), td)
   }
 
-  "InvokeTransition3" should "pass with two transitions" in {
-    val result = ModbatTestHarness.testMain(Array("-s=1", "-n=1", "--no-redirect-out", "-s",  "modbat.test.InvokeTransition3"), (() => ModbatTestHarness.setTestJar()))
-    result._1 should be(0)
-    result._2.filter(_.contains("1 tests executed, 1 ok, 0 failed.")) should not be empty
-    result._2.filter(_.contains("2 states covered (66 % out of 3),")) should not be empty
-    result._2.filter(_.contains("2 transitions covered (66 % out of 3).")) should not be empty
-    result._3 shouldBe empty
+  "InvokeTransition3" should "pass with two transitions" in { td =>
+    ModbatTestHarness.test(Array("-s=1", "-n=1", "--no-redirect-out", "-s",  "modbat.test.InvokeTransition3"), (() => ModbatTestHarness.setTestJar()), td)
   }
 
-  "InvokeTransition4" should "end with n==3" in {
-    val result = ModbatTestHarness.testMain(Array("-s=1", "-n=1", "--no-redirect-out", "-s",  "modbat.test.InvokeTransition4"), (() => ModbatTestHarness.setTestJar()))
-    result._1 should be(0)
-    result._2.filter(_.contains("1 tests executed, 1 ok, 0 failed.")) should not be empty
-    result._2.filter(_.contains("3 states covered (100 % out of 3),")) should not be empty
-    result._2.filter(_.contains("3 transitions covered (100 % out of 3).")) should not be empty
-    result._3 shouldBe empty
+  "InvokeTransition4" should "pass with n==3" in { td =>
+    val result = ModbatTestHarness.test(Array("-s=1", "-n=1", "--no-redirect-out", "-s",  "modbat.test.InvokeTransition4"), (() => ModbatTestHarness.setTestJar()), td)
   }
 }
 
