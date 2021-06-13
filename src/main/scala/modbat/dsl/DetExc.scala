1671137cb25d4b69826ca04fe7207423f490dc93
James Yoo <jyoo980@gmail.com>
2021-06-13 09:51:06 -0700

    removing unnecessary companion object

--- src/main/scala/modbat/dsl/DetExc.scala
+++ src/main/scala/modbat/dsl/DetExc.scala
@@ -3,8 +3,3 @@ package modbat.dsl
 import scala.util.matching.Regex
 
 final case class DetExc(excName: Regex, target: State, fullName: String, line: Int)
-
-object DetExc {
-  def apply(excName: Regex, target: State, fullName: String, line: Int): DetExc =
-    new DetExc(excName, target, fullName, line)
-}
